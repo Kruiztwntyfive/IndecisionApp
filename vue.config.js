0706b27c6c530a908cb5e78dc36d89d9661bd6ea
Kevin Ruiz <kevingerardoruiz.2001@gmail.com>
2023-03-21 09:58:31 -0600

    asddasda

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/Kruiztwntyfive/indecisionApp/',
-  outputDir: 'dist'
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: '/Kruiztwntyfive/indecisionApp/',
+//   outputDir: 'dist'
+// })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/indecisionApp/'
+    : '/'
+}
